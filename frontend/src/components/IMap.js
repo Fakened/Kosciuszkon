c67a648c1eb220509ff2fdbf42c91d885efebc77
Michał Ogiba <mifiszon@gmail.com>
2024-06-08 23:08:50 +0200

    LocationMark

--- frontend/src/components/IMap.js
+++ frontend/src/components/IMap.js
@@ -1,6 +1,8 @@
-import React, { useState } from "react";
-import { MapContainer, TileLayer } from "react-leaflet";
+import React from "react";
+import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
+import L from "leaflet";
 import "leaflet/dist/leaflet.css";
+import MarkIcon from '../assets/Mark.png'; 
 
 const Map = () => {
   const bounds = [
@@ -8,12 +10,37 @@ const Map = () => {
     [50.505, 21.053],
   ];
 
+  const krakowStops = [
+    { lat: 50.0647, lng: 19.945 },
+    { lat: 50.0615, lng: 19.937 },
+    { lat: 50.0511, lng: 19.935 },
+    { lat: 50.0514, lng: 19.937 },
+    { lat: 50.0467, lng: 19.948 },
+  ];
+
+  const markIcon = L.icon({
+    iconUrl: MarkIcon,
+    iconSize: [32, 32],
+    iconAnchor: [16, 32],
+    popupAnchor: [0, -32],
+  });
+
+  const renderKrakowStops = () => {
+    return krakowStops.map((stop, index) => (
+      <Marker key={index} position={[stop.lat, stop.lng]} icon={markIcon}>
+        <Popup>
+          Przystanek Kraków {index + 1}
+        </Popup>
+      </Marker>
+    ));
+  };
+
   return (
     <div className="relative h-screen w-screen overflow-hidden z-40">
       <div className="flex flex-col h-screen">
         <MapContainer
           center={[50.0615, 19.937]}
-          zoom={9}
+          zoom={12}
           minZoom={9}
           maxZoom={14}
           maxBounds={bounds}
@@ -24,8 +51,8 @@ const Map = () => {
             url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
             attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
           />
+          {renderKrakowStops()}
         </MapContainer>
-        
       </div>
     </div>
   );
