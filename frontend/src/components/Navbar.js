e7b41393173d27acab72c5853330a18d7941ef75
Michał Ogiba <mifiszon@gmail.com>
2024-06-08 23:43:23 +0200

    bg

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -1,12 +1,21 @@
-import Logo from '../assets/Logo.png'
+import Logo from '../assets/Logo.png';
+import homeBG from '../assets/home_bg2.svg';
 import React from 'react';
 
 const Navbar = () => {
   return (
-    <nav className="bg-white p-4">
+    <nav 
+      className="bg-white p-4" 
+      style={{ 
+        backgroundImage: `url(${homeBG})`, 
+        backgroundSize: 'cover',
+        backgroundRepeat: 'no-repeat',
+        backgroundPosition: 'center'
+      }}
+    >
       <div className="max-w-7xl mx-auto flex justify-between items-center">
         <div className="">
-          <img src={Logo} alt="Koleje Małopolskie" className="w-24 h-auto"></img>
+          <img src={Logo} alt="Koleje Małopolskie" className="w-24 h-auto" />
         </div>
         <div className="flex-shrink-0">
           <a href="/" className="text-blue-950 font-bold text-xl">Koleje Małopolskie - Monitor Tras</a>
