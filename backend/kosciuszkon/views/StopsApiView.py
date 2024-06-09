53f8724edcfc8c5c609e5b692e0cbf306ba6293f
Nokrad997 <zajkowskizajko@gmail.com>
2024-06-09 05:02:13 +0200

    trips rest api

--- backend/kosciuszkon/views/StopsApiView.py
+++ backend/kosciuszkon/views/StopsApiView.py
@@ -4,7 +4,7 @@ from rest_framework.response import Response
 from rest_framework import status
 from drf_yasg.utils import swagger_auto_schema
 
-from kosciuszkon.models import Stops, StopTimes
+from kosciuszkon.models import Stops, StopTimes, Trips
 from kosciuszkon.serializers import StopsSerializer, StopTimesSerializer
 
 logger = logging.getLogger(__name__)
@@ -17,15 +17,15 @@ class StopsApiView(APIView):
     def get(self, request, id=None):
         if id:
             try:
-                stop = Stops.objects.filter(stop_id=id)
-                if not stop.exists():
+                trip = Trips.objects.filter(trip_id=id)
+                if not trip.exists():
                     logger.error(f"No Stops found with given id: {id}")
-                    return Response({'error': f'No Stops found with given id: {id}'}, status=status.HTTP_404_NOT_FOUND)
+                    return Response({'error': f'No trips found with given id: {id}'}, status=status.HTTP_404_NOT_FOUND)
                 
-                stop_time = StopTimes.objects.filter(stop_id=stop[0].stop_id)
+                stop_time = StopTimes.objects.filter(trip_id=trip[0].trip_id)
                 if not stop_time.exists():
-                    logger.error(f"No StopTimes found for stop_id: {stop[0].stop_id}")
-                    return Response({'error': f'No StopTimes found for stop_id: {stop[0].stop_id}'}, status=status.HTTP_404_NOT_FOUND)
+                    logger.error(f"No StopTimes found for stop_id: {trip[0].trip_id}")
+                    return Response({'error': f'No StopTimes found for stop_id: {trip[0].trip_id}'}, status=status.HTTP_404_NOT_FOUND)
                 
                 stop_time_serializer = StopTimesSerializer(stop_time, many=True)
                 # Ensure all values are JSON compliant
