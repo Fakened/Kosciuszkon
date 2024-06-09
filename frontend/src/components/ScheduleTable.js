36ebfd74d7110b5804bf67f513dd62723cdf9a76
Michał Ogiba <mifiszon@gmail.com>
2024-06-09 05:49:46 +0200

    table

--- frontend/src/components/ScheduleTable.js
+++ frontend/src/components/ScheduleTable.js
@@ -7,8 +7,19 @@ const ScheduleTable = ({ routeId }) => {
   useEffect(() => {
     const fetchData = async () => {
       try {
-        const response = await axios.get(`http://localhost:8000/api/trips/${routeId}`);
-        setSchedule(response.data);
+        const tripsResponse = await axios.get(`http://localhost:8000/api/trips/${routeId}`);
+        const trips = tripsResponse.data;
+
+        const scheduleData = await Promise.all(trips.map(async (trip) => {
+          const stopsResponse = await axios.get(`http://localhost:8000/api/stops/${trip.trip_id}`);
+          return stopsResponse.data.map(stop => ({
+            trip,
+            stop,
+          }));
+        }));
+
+        console.log(scheduleData)
+        setSchedule(scheduleData.flat());
       } catch (error) {
         console.error('Błąd pobierania danych:', error);
       }
@@ -22,7 +33,7 @@ const ScheduleTable = ({ routeId }) => {
       <table className="w-full bg-white shadow-md rounded-lg overflow-hidden">
         <thead>
           <tr className="bg-[#385088] text-white">
-            <th className="px-4 py-2">Miejscowość</th>
+            <th className="px-4 py-2">Trasa</th>
             <th className="px-4 py-2">Strefa</th>
             <th className="px-4 py-2">Przystanek</th>
             <th className="px-4 py-2">Przyjazd</th>
@@ -30,11 +41,11 @@ const ScheduleTable = ({ routeId }) => {
         </thead>
         <tbody>
           {schedule.map((item, index) => (
-            <tr key={`${item.trip_id}`} className={index % 2 === 0 ? "bg-gray-200" : "bg-gray-100"}>
-              <td className="px-4 py-2 text-center">{item.route.route_long_name}</td>
-              <td className="px-4 py-2 text-center">{item.service.service_id}</td>
-              <td className="px-4 py-2 text-center">{item.shape_id.id}</td>
-              <td className="px-4 py-2 text-center">{item.trip_short_name}</td>
+            <tr key={index} className={index % 2 === 0 ? "bg-gray-200" : "bg-gray-100"}>
+              <td className="px-4 py-2 text-center">{item.trip.route.route_long_name}</td>
+              <td className="px-4 py-2 text-center">{item.trip.route.route_type}</td>
+              <td className="px-4 py-2 text-center">{item.stop.stop.stop_name}</td>
+              <td className="px-4 py-2 text-center">{item.stop.arrival_time}</td>
             </tr>
           ))}
         </tbody>
